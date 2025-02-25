12f6e1f96eff5b56a06b784bc31497160f12b64f
koyava-valeria <v.koyava@icloud.com>
2025-02-25 18:44:27 +0300

    models and static

--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_new = models.BooleanField(default=False)
+    discount = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    brand = models.ForeignKey("Brand", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        db_table = "products"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        db_table = "categories"
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Brand"
+        verbose_name_plural = "Brands"
+        db_table = "brands"
--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home')
+]
